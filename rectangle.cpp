2be9b42b9e6041e19cdc81c9352391e289025eff
Ubuntu <ubuntu@ip-172-31-24-85.ec2.internal>
2021-09-15 03:08:04 +0000

    rectanglecpp

---
+++ rectangle.cpp
@@ -0,0 +1,26 @@
+// Copyright 2020 MTHS
+// Author: Mr. Coxall
+// This program calculates the area and perimeter of a rectangle
+// with user input
+#include <iostream>
+int main() {
+// this function calculates area and perimeter
+int length;
+int width;
+int area;
+int perimeter;
+// input
+std::cout << "Enter length of the rectangle (mm): ";
+std::cin >> length;
+std::cout << "Enter width of the rectangle (mm): ";
+std::cin >> width;
+// process
+area = length*width;
+perimeter = 2*(length+width);
+// output
+std::cout << "" << std::endl;
+std::cout << "Area is " << area << " mm2." << std::endl;
+std::cout << "Perimeter is " << perimeter << " mm." << std::endl;
+std::cout << "" << std::endl;
+std::cout << "Done." << std::endl;
+}
